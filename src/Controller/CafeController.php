ca553e5cacb02a50dffa0974dac21bc2f68ff516
Mehdi <mehdiennajehfr@gmail.com>
2024-02-25 21:38:45 +0100

    Liste café fini

--- src/Controller/CafeController.php
+++ src/Controller/CafeController.php
@@ -5,14 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Repository\TypeCafeRepository;
 
 class CafeController extends AbstractController
 {
     #[Route('/liste-typecafe', name: 'app_liste_typecafe')]
-    public function index(): Response
+    public function listeCafe(TypeCafeRepository $typeCafeRepository): Response
     {
+        $typeCafe = $typeCafeRepository->findAll();
         return $this->render('cafe/liste-typecafe.html.twig', [
+            'typeCafe' => $typeCafe
         ]);
+    }
+    #[Route('/modifier-typecafe', name: 'app_modifier_typecafe')]
+    public function modifierTypeCafe(): Response
+    {
+   
+    return $this->render('cafe/modifier-typecafe.html.twig', [
+   
+    ]);
     }
 }
